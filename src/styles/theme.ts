3ad59572fe821f3e3f2ffcafd9c45bc95e4a41e4
dione616 <54132722+dione616@users.noreply.github.com>
2021-02-20 23:06:24 +0200

    Added TextFields and fixed buttons

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,14 @@
+import { green } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export const colors = {
   black: "#000000",
   white: "#ffffff",
+  grey: "#636363",
   success: "#6D9B12",
   primary: "#4285F4",
   default: "#E5E5E5",
+  error: "#FF0000",
 };
 export const size = {
   large: {
@@ -22,18 +25,11 @@ export const size = {
   },
 };
 
-const defaultTheme = createMuiTheme();
-
 const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
   overrides: {
-    MuiSelect: {
-      root: {
-        paddingRight: defaultTheme.spacing(10),
-      },
-      icon: {
-        color: colors.black,
-      },
-    },
     MuiButton: {
       containedSecondary: {
         backgroundColor: colors.success,
@@ -44,6 +40,7 @@ const theme = createMuiTheme({
       },
       containedPrimary: {
         backgroundColor: colors.primary,
+        color: colors.white,
         "&:hover": {
           //you want this to be the same as the backgroundColor above
           backgroundColor: colors.primary,
@@ -53,6 +50,7 @@ const theme = createMuiTheme({
         textTransform: "capitalize",
         backgroundColor: colors.primary,
         borderRadius: "6px",
+        color: colors.grey,
         "&:hover": {
           //you want this to be the same as the backgroundColor above
           backgroundColor: colors.default,
@@ -60,7 +58,8 @@ const theme = createMuiTheme({
       },
       contained: {
         boxShadow: "none",
-        marginRight: "15px",
+        marginRight: "30px",
+        color: colors.grey,
       },
       sizeLarge: {
         width: size.large.width,
@@ -73,6 +72,49 @@ const theme = createMuiTheme({
       iconSizeMedium: {
         width: size.small.width,
         height: size.small.height,
+      },
+    },
+    MuiTextField: {
+      root: {
+        width: "80%",
+      },
+    },
+    MuiTypography: {
+      colorSecondary: {
+        color: colors.success,
+      },
+      colorError: { color: colors.error },
+      subtitle2: {
+        fontSize: "16px",
+        fontWeight: 500,
+      },
+      caption: {
+        fontWeight: 1000,
+        fontSize: "14px",
+      },
+      h1: {
+        fontSize: "36px",
+        fontWeight: 900,
+      },
+      h2: {
+        fontSize: "32px",
+        fontWeight: 700,
+      },
+      h3: {
+        fontSize: "28px",
+        fontWeight: 500,
+      },
+      h4: {
+        fontSize: "24px",
+        fontWeight: 500,
+      },
+      h5: {
+        fontSize: "18px",
+        fontWeight: 500,
+      },
+      h6: {
+        fontSize: "16px",
+        fontWeight: 700,
       },
     },
   },
